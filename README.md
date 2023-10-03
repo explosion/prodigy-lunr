c1100153e1220994da14ce7bd42b49ad37f4a513
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-10-03 14:27:11 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# prodigy-lunr
-A Prodigy pluging for document search via LUNR
+<a href="https://explosion.ai"><img src="https://explosion.ai/assets/img/logo.svg" width="125" height="125" align="right" /></a>
+
+# 🌕 Prodigy-Lunr
+
+![](images/approach.png)
+
+This repository contains a Prodigy plugin for recipes that use [lunr]() as a lightweight search engine to fetch relevant subsets of the data to curate. 
+
+You can install this plugin via `pip`. 
+
+```
+pip install "prodigy-lunr @ git+https://github.com/explosion/prodigy-lunr"
+```
+
+To learn more about this plugin, you can check the [Prodigy docs]().
+
---
+++ prodigy_lunr/__init__.py
@@ -0,0 +1,145 @@
+from tempfile import NamedTemporaryFile
+import itertools as it
+from pathlib import Path
+from typing import List, Optional, Dict
+
+import srsly
+from tqdm import tqdm
+
+from prodigy import recipe
+from prodigy.recipes.textcat import manual as textcat_manual
+from prodigy.recipes.ner import manual as ner_manual
+from prodigy.recipes.spans import manual as spans_manual
+from lunr import lunr
+from lunr.index import Index
+
+
+@recipe(
+    "lunr.text.index",
+    # fmt: off
+    source=("Path to text source to index", "positional", None, str),
+    index_path=("Path of trained index", "positional", None, Path),
+    # fmt: on
+)
+def index(source: Path, index_path: Path):
+    """Builds an HSNWLIB index on example text data."""
+    # Store sentences as a list, not perfect, but works.
+    documents = [{"idx": i, **ex} for i, ex in enumerate(srsly.read_jsonl(source))]
+    # Create the index
+    index = lunr(ref='idx', fields=('text',), documents=documents)
+    # Store it on disk
+    srsly.write_gzip_json(index_path, index.serialize(), indent=0)
+
+
+@recipe(
+    "lunr.text.fetch",
+    # fmt: off
+    source=("Path to text source that has been indexed", "positional", None, str),
+    index_path=("Path to index", "positional", None, Path),
+    out_path=("Path to write examples into", "positional", None, Path),
+    query=("ANN query to run", "option", "q", str),
+    n=("Max number of results to return", "option", "n", int),
+    # fmt: on
+)
+def fetch(source: Path, index_path: Path, out_path: Path, query:str, n:int=200):
+    """Fetch a relevant subset using a HNSWlib index."""
+    if not query:
+        raise ValueError("must pass query")
+    
+    documents = [{"idx": i, **ex} for i, ex in enumerate(srsly.read_jsonl(source))]
+    index = Index.load(srsly.read_gzip_json(index_path))
+    results = index.search(query)[:n]
+
+    def to_prodigy_examples(results):
+        for res in results:
+            ex = documents[int(res['ref'])]
+            ex['meta'] = {
+                'score': res['score'], 'query': query
+            }
+            yield ex
+
+    srsly.write_jsonl(out_path, to_prodigy_examples(results))
+
+
+@recipe(
+    "textcat.lunr.manual",
+    # fmt: off
+    dataset=("Dataset to save answers to", "positional", None, str),
+    examples=("Examples that have been indexed", "positional", None, str),
+    index_path=("Path to trained index", "positional", None, Path),
+    labels=("Comma seperated labels to use", "option", "l", str),
+    query=("ANN query to run", "option", "q", str),
+    exclusive=("Labels are exclusive", "flag", "e", bool),
+    # fmt: on
+)
+def textcat_ann_manual(
+    dataset: str,
+    examples: Path,
+    index_path: Path,
+    labels:str,
+    query:str,
+    exclusive:bool = False
+):
+    """Run textcat.manual using a query to populate the stream."""
+    with NamedTemporaryFile(suffix=".jsonl") as tmpfile:
+        fetch(examples, index_path, out_path=tmpfile.name, query=query)
+        stream = list(srsly.read_jsonl(tmpfile.name))
+        return textcat_manual(dataset, stream, label=labels.split(","), exclusive=exclusive)
+
+
+@recipe(
+    "ner.lunr.manual",
+    # fmt: off
+    dataset=("Dataset to save answers to", "positional", None, str),
+    nlp=("spaCy model to load", "positional", None, str),
+    examples=("Examples that have been indexed", "positional", None, str),
+    index_path=("Path to trained index", "positional", None, Path),
+    labels=("Comma seperated labels to use", "option", "l", str),
+    query=("ANN query to run", "option", "q", str),
+    exclusive=("Labels are exclusive", "flag", "e", bool),
+    # fmt: on
+)
+def ner_ann_manual(
+    dataset: str,
+    nlp: str,
+    examples: Path,
+    index_path: Path,
+    labels:str,
+    query:str,
+    exclusive:bool = False
+):
+    """Run ner.manual using a query to populate the stream."""
+    with NamedTemporaryFile(suffix=".jsonl") as tmpfile:
+        fetch(examples, index_path, out_path=tmpfile.name, query=query)
+        stream = list(srsly.read_jsonl(tmpfile.name))
+        ner_manual(dataset, nlp, stream, label=labels, exclusive=exclusive)
+
+
+@recipe(
+    "spans.lunr.manual",
+    # fmt: off
+    dataset=("Dataset to save answers to", "positional", None, str),
+    nlp=("spaCy model to load", "positional", None, str),
+    examples=("Examples that have been indexed", "positional", None, str),
+    index_path=("Path to trained index", "positional", None, Path),
+    labels=("Comma seperated labels to use", "option", "l", str),
+    patterns=("Path to match patterns file", "option", "pt", Path),
+    query=("ANN query to run", "option", "q", str),
+    exclusive=("Labels are exclusive", "flag", "e", bool),
+    # fmt: on
+)
+def spans_ann_manual(
+    dataset: str,
+    nlp: str,
+    examples: Path,
+    index_path: Path,
+    labels:str,
+    query:str,
+    patterns: Optional[Path] = None,
+    exclusive:bool = False
+):
+    """Run spans.manual using a query to populate the stream."""
+    with NamedTemporaryFile(suffix=".jsonl") as tmpfile:
+        fetch(examples, index_path, out_path=tmpfile.name, query=query)
+        stream = list(srsly.read_jsonl(tmpfile.name))
+        spans_manual(dataset, nlp, stream, label=labels, exclusive=exclusive, patterns=patterns)
