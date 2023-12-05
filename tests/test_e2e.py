aa92c4dd2c819e89558178d3aa66a3303b715cc9
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-12-05 16:14:42 +0100

    comments

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -13,12 +13,18 @@ base_calls = [
 @pytest.mark.parametrize("query", ["benchmark", "corpus"])
 @pytest.mark.parametrize("base_call", base_calls)
 def test_basic_interactions(query, base_call):
+    """Ensure that we check e2e that the query appears when we reset the stream."""
     extra_settings = "tests/datasets/new-dataset.jsonl tests/datasets/index.gz.json --query download --allow-reset --n 20 --labels foo,bar,buz"
     with prodigy_playwright(f"{base_call} {extra_settings}") as (ctx, page):
+        # Reset the stream
         page.get_by_text("Reset stream?").click()
         page.get_by_label("New query:").click()
         page.get_by_label("New query:").fill(query)
         page.get_by_role("button", name="Refresh Stream").click()
         page.get_by_text("Reset stream?").click()
-        elem = page.locator(".prodigy-content").first
-        expect(elem).to_contain_text(query)
+        
+        # Hit accept a few times, making sure that the query appears
+        for _ in range(10):
+            page.get_by_label("accept (a)").click()
+            elem = page.locator(".prodigy-content").first
+            expect(elem).to_contain_text(query)
